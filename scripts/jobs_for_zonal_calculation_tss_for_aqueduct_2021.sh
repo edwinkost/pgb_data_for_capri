ad6f1880f2d6533f70b711f3a7383b87d1fc1dda
Edwin Sutanudjaja <edwinkost@UU105378>
2024-05-18 13:30:22 +0200

    start the calculation for w5e5

--- scripts/jobs_for_zonal_calculation_tss_for_aqueduct_2021.sh
+++ scripts/jobs_for_zonal_calculation_tss_for_aqueduct_2021.sh
@@ -1,7 +1,8 @@
 
 python zonal_calculation_tss_for_aqueduct_2021.py \
  /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/gswp3-w5e5/historical-reference/ \
- pcrglobwb_cmip6-isimip3-gswp3-w5e5_image-aqueduct_historical-reference
+ pcrglobwb_cmip6-isimip3-gswp3-w5e5_image-aqueduct_historical-reference \
  1960 2019
+
 
  
