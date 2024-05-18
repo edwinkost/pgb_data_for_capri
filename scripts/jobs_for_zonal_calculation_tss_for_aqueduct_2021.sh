8f0faa24403d38cd02dd7bb955ddfd709fe47654
Edwin Sutanudjaja <edwinkost@UU105378>
2024-05-18 14:04:13 +0200

    start the calculation for gfdl

--- scripts/jobs_for_zonal_calculation_tss_for_aqueduct_2021.sh
+++ scripts/jobs_for_zonal_calculation_tss_for_aqueduct_2021.sh
@@ -1,8 +1,31 @@
 
+#~ gswp3-w5e5 historical-reference
 python zonal_calculation_tss_for_aqueduct_2021.py \
  /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/gswp3-w5e5/historical-reference/ \
  gswp3-w5e5 historical-reference \
  1960 2019
+
+GCM_CODE="gfdl-esm4"
+SCENARIO_CODE="historical"
+python zonal_calculation_tss_for_aqueduct_2021.py \
+ /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/${GCM_CODE}/${SCENARIO_CODE}/ \
+ ${GCM_CODE} ${SCENARIO_CODE} \
+ 1960 2014
+SCENARIO_CODE="ssp126"
+python zonal_calculation_tss_for_aqueduct_2021.py \
+ /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/${GCM_CODE}/${SCENARIO_CODE}/ \
+ ${GCM_CODE} ${SCENARIO_CODE} \
+ 2015 2100
+SCENARIO_CODE="ssp370"
+python zonal_calculation_tss_for_aqueduct_2021.py \
+ /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/${GCM_CODE}/${SCENARIO_CODE}/ \
+ ${GCM_CODE} ${SCENARIO_CODE} \
+ 2015 2100
+SCENARIO_CODE="ssp585"
+python zonal_calculation_tss_for_aqueduct_2021.py \
+ /scratch/depfg/sutan101/pgb_output_for_capri_iso3_country/${GCM_CODE}/${SCENARIO_CODE}/ \
+ ${GCM_CODE} ${SCENARIO_CODE} \
+ 2015 2100
 
 #~ sutan101@gpu014.cluster:/eejit/home/sutan101$ ls /depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16_merged/g*/*/*totalRunoff*
 #~ /depfg/sutan101/pcrglobwb_wri_aqueduct_2021/pcrglobwb_aqueduct_2021_monthly_annual_files/version_2021-09-16_merged/gfdl-esm4/historical/pcrglobwb_cmip6-isimip3-gfdl-esm4_image-aqueduct_historical_totalRunoff_global_monthly-total_1960_2014_basetier1.nc
